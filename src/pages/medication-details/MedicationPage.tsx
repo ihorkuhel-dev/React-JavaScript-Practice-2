506f8f7d116cdf19759309e88bc9b20a2cbd687b
ihorkuhel-dev <ihor.kuhel@innowise.com>
2026-04-29 13:43:22 +0200

    feat: base implementation of MedicationPage.tsx

--- src/pages/medication-details/MedicationPage.tsx
+++ src/pages/medication-details/MedicationPage.tsx
@@ -1,9 +1,101 @@
+import {getRouteApi} from "@tanstack/react-router";
+import {useProductById} from "@/features/medecine/api/medicineApi.ts";
+import {GeoIcon} from "@/shared/assets/GeoIcon.tsx";
+import {Button} from "@/shared/ui/button.tsx";
+import {CalendarIcon} from "@/shared/assets/CalendarIcon.tsx";
+import {DirectionIcon} from "@/shared/assets/DeclineIcon.tsx";
+import {Badge} from "@/shared/ui/badge.tsx";
+
 export function MedicationPage() {
+
+    const routeApi = getRouteApi('/_main/medications/$id')
+    const { id } = routeApi.useParams()
+
+    const {
+        data: productData,
+    } = useProductById(id, true);
+
     return (
-        <div className="flex flex-col space-y-4 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
-                Medication
-            </h1>
+        <div className="flex gap-6">
+            <div className="flex flex-col flex-2  gap-8 ">
+                <div className="flex flex-col bg-mywhite p-4 gap-8 border border-mygrey-lighter rounded-sm">
+                    <div>
+                        <h1 className="text-3xl font-semibold text-myblack mb-2">{productData?.title}</h1>
+                        <p className="text-sm font-medium text-mygrey">{productData?.brand}</p>
+                    </div>
+                    <div className="flex gap-16 py-6 px-4 border border-mygrey-lighter">
+                        <div className="flex flex-col  gap-4">
+                            <div className="flex gap-2 items-center">
+                            <span className="bg-myaquamarine-lighter w-10 h-10 rounded-sm flex items-center justify-center">
+                                <GeoIcon color="currentColor" className="text-myaquamarine"/>
+                            </span>
+                                <h3 className="text-lg font-semibold">Location</h3>
+                            </div>
+
+                            <p className="text-mygrey text-sm font-medium">434 Rockaway Ave, ,BrooklynNew York,
+                                11212-5636</p>
+                        </div>
+
+                        <div className="flex flex-col  gap-4">
+                            <div className="flex gap-2 items-center">
+                            <span className="bg-myaquamarine-lighter w-10 h-10 rounded-sm flex items-center justify-center">
+                                <GeoIcon color="currentColor" className="text-myaquamarine"/>
+                            </span>
+                                <h3 className="text-lg font-semibold">Date & Time</h3>
+                            </div>
+
+                            <p className="text-mygrey text-sm font-medium">28th June - 2nd July 2022
+                                10 am - 4 pm Eastern Daylight Time </p>
+                        </div>
+                    </div>
+                    <div className="flex gap-4">
+                        <Button className="flex-1">Start Process</Button>
+                        <Button className="flex-1"
+                                variant="secondary">
+                            <CalendarIcon color="currentColor" className="mr-1" /> Add to Calendar</Button>
+
+                    </div>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <h2 className="text-3xl font-semibold text-myblack">About this event</h2>
+                    <p className="text-sm font-medium text-mygrey">{productData?.description}</p>
+                </div>
+            </div>
+            <div className='flex flex-col flex-1 gap-10 border-l border-mygrey-lighter px-6'>
+                <div className="flex flex-col gap-4">
+                    <h3 className="text-2xl font-semibold">Manufacturer</h3>
+                    <p className="text-sm font-medium text-mygrey">Serenity Health Clinic</p>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <h3 className="text-2xl font-semibold">Location</h3>
+
+                    <address className="flex gap-4">
+                        <p className="text-sm font-medium text-myblack">434 Rockaway Ave, 11212-5636</p>
+                        <p className="text-sm font-medium text-mygrey">Brooklyn New York</p>
+                    </address>
+
+                    <Button variant='secondary'><DirectionIcon color="currentColor"/> Get Directions</Button>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <h3 className="text-2xl font-semibold">Tags</h3>
+                    <div className="flex gap-4 ">
+                        {productData?.tags ? (
+                            productData?.tags.map(tag => (
+                                <Badge key={tag} variant="secondary" className="capitalize">{tag}</Badge>
+                            ))
+                        )
+                            : (
+                                <>
+                                    <Badge variant="secondary" className="capitalize">Medicine #459026</Badge>
+                                    <Badge variant="secondary" className="capitalize">Vaccine #78</Badge>
+                                </>
+                            )
+                        }
+
+                    </div>
+                </div>
+            </div>
         </div>
+
     )
 }
