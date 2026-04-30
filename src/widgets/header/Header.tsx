e94a94c546e8a94674f6830b2010013c252833a3
ihorkuhel-dev <ihor.kuhel@innowise.com>
2026-04-30 12:33:28 +0200

    feat: change switch-theme logic, add ThemeContext

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -10,8 +10,14 @@ import {NAV_BUTTON, NAV_LINK} from "@/shared/config/header.ts";
 import {Button} from "@/shared/ui/button.tsx";
 import "./Header.scss"
 import { appDispatch } from '@/shared/lib/dispatch'
+import {useTheme} from "@/shared/lib/ThemeContext.tsx";
+import { MoonIcon } from "@/shared/assets/MoonIcon";
+import { SunIcon } from "@/shared/assets/SunIcon";
 
 export function Header() {
+
+    const {theme} = useTheme()
+
     return (
         <header  className="fixed w-full p-3 bg-mywhite border-mygrey-lighter border-b grid grid-cols-3 items-center z-50">
             <div></div>
@@ -40,13 +46,34 @@ export function Header() {
                         return(
                             <NavigationMenuItem key={button.id} className="text-mygrey-darker">
                                 <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                                    <Button
-                                    variant="circle"
-                                    className=" text-mygrey-darker hover:text-myblack hover:bg-mygrey-light"
-                                    onClick={() => appDispatch.dispatch(button.onClick as 'logout' | 'switch-theme')}
-                                    >
-                                        {Icon && <Icon aria-hidden="true" color="currentColor" className="size-5"/>}
-                                    </Button>
+                                        {button.onClick === 'switch-theme' ? (
+                                            theme === 'dark' ? (
+                                                <Button
+                                                    variant="circle"
+                                                    className=" text-mygrey-darker hover:text-myblack hover:bg-mygrey-light"
+                                                    onClick={() => appDispatch.dispatch('switch-theme')}
+                                                >
+                                                <MoonIcon aria-hidden="true" color="currentColor" className="size-5 "/>
+                                                </Button>
+                                            ):(
+                                                <Button
+                                                    variant="circle"
+                                                    className="text-myorange-darker bg-myorange-lighter"
+                                                    onClick={() => appDispatch.dispatch('switch-theme')}
+                                                >
+                                                <SunIcon aria-hidden="true" color="currentColor" className="size-5 text-myorange-darker "/>
+                                                </Button>
+                                                    )
+                                        ) : (
+                                            <Button
+                                                variant="circle"
+                                                className=" text-mygrey-darker hover:text-myblack hover:bg-mygrey-light"
+                                                onClick={() => appDispatch.dispatch(button.onClick as 'logout' | 'switch-theme')}
+                                            >
+                                                {Icon && <Icon aria-hidden="true" color="currentColor" className="size-5"/>}
+                                            </Button>
+                                            )
+                                        }
                                 </NavigationMenuLink>
                             </NavigationMenuItem>
                         )
