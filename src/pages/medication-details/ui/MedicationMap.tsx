cca276de9ea63f9a00e9239f100d40431e61e7c4
qwerty0zero <igoricloud98@gmail.com>
2026-05-03 20:54:44 +0200

    fix: fix ts errors

--- src/pages/medication-details/ui/MedicationMap.tsx
+++ src/pages/medication-details/ui/MedicationMap.tsx
@@ -6,7 +6,7 @@ import {useTheme} from "@/shared/lib/ThemeContext.tsx";
 export default function MedicationMap() {
 
     const mapRef = useRef<HTMLDivElement>(null);
-    const mapInstanceRef = useRef<Map>(null);
+    const mapInstanceRef = useRef<L.Map | null>(null);
     const {theme} = useTheme();
     const styleApi = theme == 'light' ? 'streets-v12' : 'dark-v11'
     const API_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN
@@ -23,9 +23,7 @@ export default function MedicationMap() {
             L.tileLayer(
                 `https://api.mapbox.com/styles/v1/mapbox/${styleApi}/tiles/256/{z}/{x}/{y}@2x?access_token=${API_TOKEN}`,
                 {
-                    maxZoom: 19,
-                    preferCanvas: true,
-                    zoomControl: false
+                    maxZoom: 19
                 }
             ).addTo(map);
 
@@ -38,7 +36,7 @@ export default function MedicationMap() {
         return() => {
             if (mapInstanceRef.current){
                 mapInstanceRef.current.remove()
-                mapInstanceRef.current = undefined;
+                mapInstanceRef.current = null;
             }
         }
     }, [styleApi])
--- src/shared/lib/useMediaQuery.tsx
+++ src/shared/lib/useMediaQuery.tsx
@@ -1,21 +1,21 @@
 import {useEffect, useState} from "react";
 
-export function useMediaQuery(query) {
-    const getMatches = (query) => {
+export function useMediaQuery(query: string) {
+    const getMatches = (query: string) => {
         if (typeof window !== 'undefined') {
             return window.matchMedia(query).matches;
         }
         return false;
     };
 
-    const [matches, setMatches] = useState(() => getMatches(query));
+    const [matches, setMatches] = useState<boolean>(() => getMatches(query));
 
     useEffect(() => {
         const mediaQueryList = window.matchMedia(query);
 
         setMatches(mediaQueryList.matches);
 
-        const documentChangeHandler = (e) => {
+        const documentChangeHandler = (e: MediaQueryListEvent) => {
             setMatches(e.matches);
         };
 
