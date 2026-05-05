3ffbdb0990faf3e297058e522770e6cb3376c9c1
qwerty0zero <igoricloud98@gmail.com>
2026-05-05 09:58:06 +0200

    just push

--- src/shared/ui/select.tsx
+++ src/shared/ui/select.tsx
@@ -40,6 +40,7 @@ function SelectTrigger({
   return (
     <SelectPrimitive.Trigger
       data-slot="select-trigger"
+      aria-label="select-trigger"
       data-size={size}
       className={cn(
         "flex cursor-pointer w-fit items-center justify-between gap-1.5 rounded-sm  border border-mygrey-dark bg-mywhite py-2 pr-2 pl-2.5 text-sm whitespace-nowrap shadow-xs transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive  data-placeholder:text-muted-foreground data-[size=default]:h-9 data-[size=sm]:h-8 *:data-[slot=select-value]:line-clamp-1 *:data-[slot=select-value]:flex *:data-[slot=select-value]:items-center *:data-[slot=select-value]:gap-1.5 dark:bg-input/30 dark:hover:bg-input/50 dark:aria-invalid:border-destructive/50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
